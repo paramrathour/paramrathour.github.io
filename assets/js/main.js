877a020f5bac224ba5076b42039654fdcab889a1
Param Rathour <rathourparam3435@gmail.com>
2022-11-14 20:00:37 +0530

    Updated Sidebar and Disabled reCAPTCHA

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,27 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }    
+// Rescale reCaptcha
+$(function(){
+  function rescaleCaptcha(){
+    var width = $('.g-recaptcha').parent().width();
+    var scale;
+    if (width < 302) {
+      scale = width / 302;
+    } else{
+      scale = 1.0; 
+    }
+
+    $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+    $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+    $('.g-recaptcha').css('transform-origin', '0 0');
+    $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+  }
+
+  rescaleCaptcha();
+  $( window ).resize(function() { rescaleCaptcha(); });
+
+});
 // Light/Dark Mode Toggle
 if(window.localStorage.getItem("mode")=="dark"){
     darkmode();
